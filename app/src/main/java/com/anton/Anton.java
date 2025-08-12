ecd2ffe4b833784a9ac8a7d574adb4d910c88db3
M-SaaD-H <mohd.saadhaider@gmail.com>
2025-08-12 20:15:41 +0530

    Initial Commit

---
+++ app/src/main/java/com/anton/Anton.java
@@ -0,0 +1,48 @@
+package com.anton;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.anton.db.DBService;
+
+public class Anton {
+  public static void main(String[] args) {
+    DBService db = new DBService("storage");
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    while (true) {
+      try {
+        String query = br.readLine();
+        String[] queryParts = query.split(" ");
+
+        switch (queryParts[0]) {
+          case "CREATE":
+            System.out.println(query.substring(queryParts[0].length()) + " entry created");
+            break;
+          
+          case "DELETE":
+            System.out.println(query.substring(queryParts[0].length()) + " entry deleted");
+            break;
+
+          case "SELECT":
+            System.out.println(query.substring(queryParts[0].length()) + " entry selected");
+            break;
+          
+          default:
+            System.out.println("Invalid query");
+            br.close();
+            break;
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        try {
+          br.close();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+}
